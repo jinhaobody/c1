f8a182b24b847dc85ffa61770d373e12efa99481
zhaojinhao <2481406659@qq.com>
2019-01-15 11:21:46 +0800

    第一次保存

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int qian=0;
+    printf("jinrucunqianquqianxitong");
+while(1){
+    int a=0;
+    printf("1---cunqian");
+    printf("1---cunqian");
+}
+    return 0;
+
+}
